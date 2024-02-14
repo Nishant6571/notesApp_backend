23a0f8cb5b7a6bbe916b721ecbc0ebee9e89027a
Nishant6571 <nishantbhargava.bhargava@gmail.com>
2024-02-15 01:31:07 +0530

    update

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { connection } = require("./config/db");
 const { userRouter } = require("./routes/user.routes");
 const { noteRouter } = require("./routes/note.routes");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use("/users", userRouter);
 app.use("/notes", noteRouter);
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.status(200).send({ msg: "Home page" });
